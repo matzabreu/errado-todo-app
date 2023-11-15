3ac2a304948e9d4219942f85a8615e44817fbb6c
matzabreu <abreumagalhaes@outlook.com>
2023-11-15 19:20:33 -0300

    criação - primeira view do projeto

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require("express")
+const exphbs = require("express-handlebars")
 
 const app = express()
 
+app.engine('handlebars', exphbs.engine())
+app.set('view engine', 'handlebars')
+
 app.get('/', (requisicao, resposta) => {
-    resposta.send("Olá, mundo")
+    resposta.render('home')
 })
 
 app.listen(3000, () => {
